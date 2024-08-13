51ae4108797852384fd41a6cecfd5dc4ec7b7c66
Muhammad Umer <umerasif924@gmail.com>
2024-08-13 04:31:04 -0500

    added mongodb connectivity

--- backend/src/modules/main/app.module.ts
+++ backend/src/modules/main/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '../auth';
+import { ConfigModule, ConfigService } from '../config';
+import { UserModule } from '../user';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_TYPE'),
+          url: configService.get('DB_CONNECTION_URL'),
+          entities: [__dirname + './../**/**.entity{.ts,.js}'],
+          migrations: [__dirname + '../migrations/*{.ts,.js}'],
+          synchronize: configService.get('DB_SYNC') === 'true',
+        } as TypeOrmModuleAsyncOptions;
+      },
+    }),
+    ConfigModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
