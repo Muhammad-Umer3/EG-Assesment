a2b312ae453c1e51502b5b4665e5fa5ba63f3be6
Muhammad Umer <umerasif924@gmail.com>
2024-08-13 04:30:26 -0500

    added swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
+import { AppModule } from './modules/main/app.module';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3000);
 }
 bootstrap();
